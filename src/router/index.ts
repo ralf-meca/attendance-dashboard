c2914e2d66323d1d76e0b70f4ec6410714220e4c
ralf.meca <ralfmeca@hotmail.com>
2025-11-16 22:50:37 +0100

    Login pushback / redirection
    Added check and redirection if user doesn't have the authentication token

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/views/LoginPage.vue'
+import { useAuthStore } from '@/stores/authStore.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,22 @@ const router = createRouter({
       meta: { requiresAuth: true },
     },
   ],
+})
+
+// Navigation guard to protect routes
+router.beforeEach((to, _, next) => {
+  const authStore = useAuthStore()
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !authStore.isAuthenticated) {
+    // Redirect to "login" if the route requires auth and the user is not authenticated
+    next('/login')
+  } else if (to.path === '/login' && authStore.isAuthenticated) {
+    // Redirect to the dashboard if already logged in and trying to access login
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
